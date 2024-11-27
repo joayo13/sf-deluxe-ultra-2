03a94a6c10ef78502bb4bf13353ae9a204943444
Jordan Ayotte <jordanayotte13@outlook.com>
2024-11-27 01:51:43 -0500

    added base of experience system, added bg to paralax layer

--- global_scripts/Game.gd
+++ global_scripts/Game.gd
@@ -1,3 +1,14 @@
 extends Node
 
 var gold: int = 0
+var player_level: int = 0
+var level_experience: int = 0
+var level_experience_max: int = 100
+signal level_up
+
+func add_experience(experience_amount: int):
+	level_experience += experience_amount
+	if level_experience >= level_experience_max:
+		level_experience = level_experience - level_experience_max
+		emit_signal("level_up", player_level)
+	
